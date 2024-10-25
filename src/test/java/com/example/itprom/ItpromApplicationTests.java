09914215146c9480bcdf9dc1c89128f55c167568
Kreoin <steamuser85@yandex.ru>
2024-10-25 11:05:33 +0300

    first commit

---
+++ src/test/java/com/example/itprom/ItpromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itprom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
