20269fb76f13569d1d5f442136edc93954d75dde
Kreoin <steamuser85@yandex.ru>
2024-10-26 10:48:57 +0300

    update repositories: add find by name methods

--- src/main/java/com/example/itprom/repository/ProfessionRepository.java
+++ src/main/java/com/example/itprom/repository/ProfessionRepository.java
@@ -1,5 +1,7 @@
 package com.example.itprom.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.itprom.model.Profession;
 
 @Repository
 public interface ProfessionRepository extends JpaRepository<Profession, Long> {
+    Optional<Profession> findByName(String name);
 }
